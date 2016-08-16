b713115993e133a49e45c74281d31644aa428b63
吳剛志 <chicken@chicken-house.net>
2016-08-17 01:46:53 +0800

    update

--- Containers/Web/build.cmd
+++ Containers/Web/build.cmd
@@ -2,4 +2,14 @@
 
 
 docker build -t=andrew/mvcdemo:latest .
+
+: docker run --name demo1 -d -p 81:80 andrew/mvcdemo
+: docker run --name demo2 -d -p 82:80 andrew/mvcdemo
+
 :: docker run --name demo -d -p 80:80 andrew/mvcdemo
+
+
+
+
+docker build -t=andrew/revproxy:latest ..\RevProxy(Nginx)
+docker run --name proxy -d -p 80:80 andrew/revproxy
